bf5130c52b4e934fe60e8ceec3f2183cb950b61c
Jeff Gerber <jallengerber@gmail.com>
2016-04-25 17:00:26 -0700

    modified server * endpoint

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/images_dev');
 var imagesRouter = require(__dirname + '/routes/images_routes');
 app.use('/api', imagesRouter);
 
+app.use(express.static('./'));
+
+app.get('/', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('/app/index.html', { root: '.' });
+});
+
+
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('server up on port: ' + port);
